7599467d5e65cfb76d2a3e20c0ccb82fb8933068
Philip Newborough <corenominal@corenominal.org>
2026-03-06 10:56:05 +0000

    URLs config

---
+++ app/Config/Urls.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Urls extends BaseConfig
+{
+    public $tld = '';
+    public $assets = '';
+    public $sendmail = '';
+    public $notifications = '';
+    public $logs = '';
+    public $license = '';
+    public $github = '';
+    public $tldcookiedomain = '';
+    public $cookiedomain = '';
+}
