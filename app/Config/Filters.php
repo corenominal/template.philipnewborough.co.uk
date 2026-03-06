526e33d23e9f109cc14f796568f5150225cb8649
Philip Newborough <corenominal@corenominal.org>
2026-03-06 11:59:17 +0000

    Configure filters

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -2,6 +2,10 @@
 
 namespace Config;
 
+use App\Filters\AdminFilter;
+use App\Filters\DebugFilter;
+use App\Filters\ApiFilter;
+use App\Filters\AuthFilter;
 use CodeIgniter\Config\Filters as BaseFilters;
 use CodeIgniter\Filters\Cors;
 use CodeIgniter\Filters\CSRF;
@@ -25,6 +29,10 @@ class Filters extends BaseFilters
      * or [filter_name => [classname1, classname2, ...]]
      */
     public array $aliases = [
+        'adminfilter'   => AdminFilter::class,
+        'debugfilter'   => DebugFilter::class,
+        'apifilter'     => ApiFilter::class,
+        'authfilter'    => AuthFilter::class,
         'csrf'          => CSRF::class,
         'toolbar'       => DebugToolbar::class,
         'honeypot'      => Honeypot::class,
@@ -75,6 +83,13 @@ class Filters extends BaseFilters
             // 'honeypot',
             // 'csrf',
             // 'invalidchars',
+            'authfilter' => ['except' => [
+                '/',
+                'cli/*',
+                'api/*',
+                'unauthorised',
+                ],
+            ],
         ],
         'after' => [
             // 'honeypot',
@@ -106,5 +121,9 @@ class Filters extends BaseFilters
      *
      * @var array<string, array<string, list<string>>>
      */
-    public array $filters = [];
+    public array $filters = [
+        'apifilter' => ['before' => ['api/*', 'api']],
+        'debugfilter' => ['before' => ['debug/*', 'debug']],
+        'adminfilter' => ['before' => ['admin/*', 'admin']],
+    ];
 }
