a06c2210a8b80a3a37e4c4eab43f2e55fcaf3863
Philip Newborough <corenominal@corenominal.org>
2026-03-06 11:59:38 +0000

    Update routes for test controllers

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// API routes
+$routes->match(['get', 'options'], '/api/test/ping', 'Api\Test::ping');
+
+// Command line routes
+$routes->cli('cli/test/index/(:segment)', 'CLI\Test::index/$1');
+$routes->cli('cli/test/count', 'CLI\Test::count');
