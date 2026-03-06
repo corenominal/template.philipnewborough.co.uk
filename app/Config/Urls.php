20fb82cce4eca685fc92195eaa9ca319d1b9a382
Philip Newborough <corenominal@corenominal.org>
2026-03-06 11:05:26 +0000

    Add auth url

--- app/Config/Urls.php
+++ app/Config/Urls.php
@@ -7,6 +7,7 @@ use CodeIgniter\Config\BaseConfig;
 class Urls extends BaseConfig
 {
     public $tld = '';
+    public $auth = '';
     public $assets = '';
     public $sendmail = '';
     public $notifications = '';
