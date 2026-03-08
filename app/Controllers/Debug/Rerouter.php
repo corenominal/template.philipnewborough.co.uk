5028efecacd04ae96e3f861ad611f9bb8a90eea1
Philip Newborough <corenominal@corenominal.org>
2026-03-08 10:52:44 +0000

    Show 404 if debug controller or method does not exist

--- app/Controllers/Debug/Rerouter.php
+++ app/Controllers/Debug/Rerouter.php
@@ -7,23 +7,28 @@ class Rerouter extends BaseController
     /**
      * Reroutes the request to a specified controller and method dynamically.
      *
-     * @param string      $controller The name of the controller to reroute to.
-     *                                This should correspond to a class name within the
-     *                                `App\Controllers\Debug` namespace.
-     * @param string|bool $method     (Optional) The name of the method to call on the controller.
-     *                                If not provided, the `index` method of the controller will be called.
+     * @param string      $controller The controller class name (within the `App\Controllers\Debug` namespace).
+     * @param string|bool $method     The method to call on the controller. Defaults to `index` if not provided.
      *
      * @return mixed The result of the called controller method.
      *
-     * @throws \Error If the specified controller class does not exist.
-     * @throws \Error If the specified method does not exist on the controller.
+     * @throws \CodeIgniter\Exceptions\PageNotFoundException If the controller class or method does not exist.
      */
     public function reroute($controller, $method = false)
     {
         $class = ucwords($controller);
         $class = 'App\\Controllers\\Debug\\' . $class;
 
+        if (! class_exists($class)) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException();
+        }
+
         $controller = new $class();
+
+        if ($method && ! method_exists($controller, $method)) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException();
+        }
+
         if (! $method) {
             return $controller->index();
         }
